a758b71fe8ca5f9e23b5b06bd9a7332ed933d7b4
STARS <starstarstar2015@outlook.com>
2021-09-26 11:55:03 +0800

    xmp生成 @requestbody测试

--- src/main/webapp/static/js/mybook.js
+++
@@ -1,6 +0,0 @@
-function select_sing(){
-   let select1= document.getElementById("contain");
-   let select2= document.getElementById("contain-off");
-   select1.id="contain-off";
-   select2.id="contain";
-}
